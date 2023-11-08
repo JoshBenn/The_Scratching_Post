a8ff3f95457141d7bc91888277bb7beb1074e864
Joshua Benn <jdbenn@asu.edu>
2023-11-08 08:24:04 -0700

    Basic IO test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![allow(non_snake_case)]
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("input a line, yo");
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Could not read input");
+
+    
+        println!("Your input: {}", input_text);
 }
