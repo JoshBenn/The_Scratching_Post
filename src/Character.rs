a138616c5267a9c62a5e913742448cf0a7724d7c
Joshua Benn <jdbenn@asu.edu>
2023-11-08 17:51:38 -0700

    Thank you to reddit user /u/KingofGamesYami for providing a solution to the issue.  Added necessary filepath identifier in main and readjusted all settings back as necessary.

--- src/Character.rs
+++ src/Character.rs
@@ -1,33 +1,32 @@
-mod character {
-    pub struct Character {
-        race: Race,
-        class: Class,
-        health: f64,
-        experience: u32,
-        level: u8,
-    }
+pub struct Character {
+    race: Race,
+    class: Class,
+    health: f64,
+    experience: u32,
+    level: u8,
+}
 
-    pub enum Race {
-        Cat,
-        Demon,
-        Human,
-    }
-    
-    pub enum Class {
-        Physical,
-        Magical,
-        Ranged,
-    }
+pub enum Race {
+    Cat,
+    Demon,
+    Human,
+}
 
-    impl Character {
-        pub fn new(race: Race, class: Class) -> Character {
-            Character {
-                race: race,
-                class: class,
-                health: 10.0,
-                experience: 0,
-                level: 1,
-            }
+pub enum Class {
+    Physical,
+    Magical,
+    Ranged,
+}
+
+impl Character {
+    pub fn new(race: Race, class: Class) -> Character {
+        Character {
+            race: race,
+            class: class,
+            health: 10.0,
+            experience: 0,
+            level: 1,
         }
     }
 }
+
